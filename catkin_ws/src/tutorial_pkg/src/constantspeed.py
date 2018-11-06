052337db509b5cf3fbfe0bc160ed2de777856d1c
xiaodiyyyyy <36013859+xiaodiyyyyy@users.noreply.github.com>
2018-11-06 23:05:49 +0100

    Update constantspeed.py

--- catkin_ws/src/tutorial_pkg/src/constantspeed.py
+++ catkin_ws/src/tutorial_pkg/src/constantspeed.py
@@ -2,15 +2,16 @@
 
 import rospy
 from  low_level_interface.msg import lli_ctrl_request
-pub = rospy.Publisher('/lli/ctrl_request',llictrl_request,queue_size=1)
-
+rospy.init_node('cs_publisher')
+pub = rospy.Publisher('/lli/ctrl_request',lli_ctrl_request,queue_size=1)
+rate = rospy.Rate(50)
 control_request = lli_ctrl_request()
 control_request.velocity = 30
-theta=0
-for theta in range(0,100)
+theta=50
+while not rospy.is_shutdown
 	control_request.steering = theta
 	pub.publish(control_request)
-	theta +=10
+	rate.sleep()
 
 
 
