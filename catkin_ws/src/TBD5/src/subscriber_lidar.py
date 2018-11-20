53e0d93dc36d753e1498d72b0bba1ed55e92cb9b
FedeFerro <fferro@kth.se>
2018-11-20 17:43:15 +0100

    modifications

--- catkin_ws/src/TBD5/src/subscriber_lidar.py
+++ catkin_ws/src/TBD5/src/subscriber_lidar.py
@@ -10,32 +10,32 @@ from geometry_msgs.msg import PoseArray
 rospy.init_node('laser_information')
 pub = rospy.Publisher('angle_distance_obstacles', PoseArray, queue_size=1)
 
+#yaw_list =[]
+#def callback_mocap(odometry_msg):
+    #global yaw_list
+    #yaw_list.append(odometry_msg.pose.pose.orientation.z)
+
 def callback_laserscan(scan):
     print('laserscan')
-    if not len(yaw_list)==0:
+    #if not len(yaw_list)==0:
 	#distance_list = []
-	#angle_list = []
+     angle_list = []
 
-        for i in range(len(scan.ranges)):
-            robot_yaw = yaw_list[-1]
-            angle = scan.angle_min + robot_yaw + i * scan.angle_increment
+     for i in range(len(scan.ranges)):
+           #robot_yaw = yaw_list[-1]
+           angle = scan.angle_min + i * scan.angle_increment
 	   # distance_list.append(scan.ranges[i])
-	   # angle_list.append(angle)
+	   angle_list.append(angle)
 
 	#for i in range(len(distance_list)):
 	    laserscan_data = Pose()
-	    laserscan_data.position.x = scan.raneges[i]
-	    laserscan_data.position.y = angle
+	    laserscan_data.position.x = scan.ranges[i]
+	    laserscan_data.position.y = angle_list
 
 	    pub.publish(laserscan_data)
 
-yaw_list =[]
-def callback_mocap(odometry_msg):
-    global yaw_list
-    yaw_list.append(odometry_msg.pose.pose.orientation.z)
 
-
-sub = rospy.Subscriber('/odometry_body_frame', Odometry, callback_mocap)
+#sub = rospy.Subscriber('/odometry_body_frame', Odometry, callback_mocap)
 sub = rospy.Subscriber('/scan', LaserScan, callback_laserscan)
 
 rospy.spin()
