2c7794e0b1f659756f4f67b64a53883d477348f7
helgid <helgidavids@gmail.com>
2018-11-28 16:34:32 +0100

    visualization working

--- catkin_ws/src/TBD5/src/lidar_visual.py
+++ catkin_ws/src/TBD5/src/lidar_visual.py
@@ -9,7 +9,7 @@ import random
 from sensor_msgs.msg import LaserScan
 
 x = np.linspace(-np.pi/2, np.pi/2, 721)
-y=np.random.random_integers(1, 20, 721)
+y=np.random.random_integers(0, 5, 721)
 
 plt.ion()
 fig = plt.figure()
@@ -31,23 +31,25 @@ line1, = ax.plot(x, y, 'b-')
 def callback_lidar(scan):
 
 
-	ranges = scan.ranges
-	
-	angle_min = scan.angle_min
-	increment = scan.angle_increment
-	line1.set_ydata(ranges)
-	fig.canvas.draw()
-	print(ranges[360])
+    ranges = scan.ranges
+
+    angle_min = scan.angle_min
+    increment = scan.angle_increment
+    line1.set_ydata(scan.ranges)
+    fig.canvas.draw()
+    #print(scan.ranges[360])
+    print(scan.ranges)
 
 
 rospy.init_node('lidar_visual_node')
+rate=rospy.Rate(1)
 #rospy.spin()
 #plt.show(block=True)
 def main():
 
     lidar_sub = rospy.Subscriber('/scan', LaserScan, callback_lidar)
     plt.show(block=True)
-	
+    rate.sleep()
 
 if __name__ == '__main__':
-	main()
+    main()
