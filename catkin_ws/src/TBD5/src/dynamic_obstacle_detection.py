1bfc3cdeddf8024f0e905217bc2c0e0d4f7f0a67
helgid <helgidavids@gmail.com>
2018-11-30 14:34:09 +0100

    after demo commit

--- catkin_ws/src/TBD5/src/dynamic_obstacle_detection.py
+++ catkin_ws/src/TBD5/src/dynamic_obstacle_detection.py
@@ -8,10 +8,12 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 
+import pdb
+
 rospy.init_node('dynamic_obstacle_detection')
 dynamic_scan_pub = rospy.Publisher('dynamic_scan', LaserScan, queue_size=1)
 
-resolution = 0.01
+resolution = 0.50
 
 curr_scan=[]
 ranges_global=[]
@@ -35,24 +37,24 @@ def callback_mocap(odometry_msg):
 
 
     # if not len(ranges) == 0:
-	# x_pos = odometry_msg.pose.pose.position.x
+        # x_pos = odometry_msg.pose.pose.position.x
         # y_pos = odometry_msg.pose.pose.position.y
        # # x_list=[]
         # #y_list=[]
         # #d_list=[]
 
-	# for i in range(len(ranges)):
-		# o_distance = ranges[i]
-		# o_angle = angle_min + i * increment
-		# x_o = np.cos(o_angle) * o_distance
-		# y_o = np.sin(o_angle) * o_distance
+        # for i in range(len(ranges)):
+                # o_distance = ranges[i]
+                # o_angle = angle_min + i * increment
+                # x_o = np.cos(o_angle) * o_distance
+                # y_o = np.sin(o_angle) * o_distance
                 # #TODO: rotate to yaw
-		# x_g = x_o + x_pos
-		# y_g = y_o + y_pos
+                # x_g = x_o + x_pos
+                # y_g = y_o + y_pos
                 # #TODO: check changes on x y coordinates
-		# d = math.sqrt(math.pow(x_g, 2) + math.pow(y_g, 2))
-		# x_list.append(x_g)
-		# y_list.append(y_g)
+                # d = math.sqrt(math.pow(x_g, 2) + math.pow(y_g, 2))
+                # x_list.append(x_g)
+                # y_list.append(y_g)
                     # d_list.append(d)
                # print (len(d_1))
        # x,y,x_n,y_n = obstacle_detection(t,x_list,y_list,d_list)
@@ -61,12 +63,10 @@ def callback_mocap(odometry_msg):
     global ranges_global
     global ranges_global_discrete
     global old_ranges_global_discrete
-    old_ranges_global_discrete = np.zeros((720,2))
 
     car_x = odometry_msg.pose.pose.position.x
     car_y = odometry_msg.pose.pose.position.y
     car_yaw = odometry_msg.pose.pose.orientation.z
-    print("mocap")
 
     def scan_to_rel_poses(scan):
         poses = []
@@ -78,11 +78,16 @@ def callback_mocap(odometry_msg):
 
         curr_angle = angle_min
         for dist in ranges:
-            if dist > scan.range_min and dist < scan.range_max:
-                rel_x, rel_y = dist *math.cos(curr_angle), dist * math.sin(curr_angle)
-                poses.append([rel_x, rel_y])
+            # if dist == float("inf"):
+            if dist > scan.range_max:
+                dist = scan.range_max
+            if dist < scan.range_min:
+                dist = scan.range_min
+            if dist < scan.range_min or dist > scan.range_max:
+                print(dist)
+            rel_x, rel_y = dist * math.cos(curr_angle), dist * math.sin(curr_angle)
+            poses.append([rel_x, rel_y])
             curr_angle += angle_increment
-            print(poses)
         return np.array(poses)
 
     def rotate(origin, pt, angle):
@@ -98,7 +103,6 @@ def callback_mocap(odometry_msg):
                            math.cos(angle) * (dist_y)
         return [rot_x, rot_y]
 
-
     # convert scans to body cartesian coordinates
     scan_rel_poses = scan_to_rel_poses(curr_scan)
 
@@ -111,30 +115,34 @@ def callback_mocap(odometry_msg):
 
     ranges_global = scan_poses
 
-
     ranges_global_discrete = scan_poses / resolution
     for i in range(ranges_global_discrete.shape[0]):
         for j in range(ranges_global_discrete.shape[1]):
-            ranges_global_discrete[i, j] = int(ranges_global_discrete[i,j])
+            # try:
+            # ranges_global_discrete[i, j] = int(ranges_global_discrete[i,j])
+            ranges_global_discrete[i, j] = math.floor(ranges_global_discrete[i,j])
 
     # compare old with new ranges_global_discrete
     #if old_ranges_global_discrete not [0,0]:
-    print(len(ranges_global_discrete))
-    diff = ranges_global_discrete - old_ranges_global_discrete
-    nonzero_indices = []
-    for i in range(diff.shape[0]):
-        if not diff[i, :] == 0 or not diff[i, :] == 0:
-            nonzero_indices.append(i)
+    # print(len(ranges_global_discrete))
+    if not len(old_ranges_global_discrete) == 0:
 
-    dynamic_distance = curr_scan.ranges
-    dynamic_distance = list(dynamic_distance)
-    for i in range(len(dynamic_distance)):
-        if not i in nonzero_indices:
-            dynamic_distance[i] = float("inf")
+        diff = ranges_global_discrete - old_ranges_global_discrete
+        nonzero_indices = []
+        for i in range(diff.shape[0]):
+            if not diff[i, 0] == 0.0 or not diff[i, 1] == 0.0:
+                nonzero_indices.append(i)
 
-    dynamic_scan = LaserScan()
-    dynamic_scan.ranges=dynamic_distance
-    dynamic_scan_pub.publish(dynamic_scan)
+        dynamic_scan = curr_scan
+        # dynamic_distance = curr_scan.ranges
+        dynamic_ranges_list = list(dynamic_scan.ranges)
+        for i in range(len(dynamic_ranges_list)):
+            if not i in nonzero_indices:
+                dynamic_ranges_list[i] = float("inf")
+
+        # dynamic_scan = LaserScan()
+        dynamic_scan.ranges = tuple(dynamic_ranges_list)
+        dynamic_scan_pub.publish(dynamic_scan)
     #else:
      #   old_ranges_global_discrete = ranges_global_discrete
 
@@ -177,21 +185,21 @@ def obstacle_detection(t,x_list,y_list,d_list):
     global d_2
     print(t)
     if 0.05 < t < 0.075:
-	x_1 = x_list
-	y_1 = y_list
-	d_1 = d_list
+        x_1 = x_list
+        y_1 = y_list
+        d_1 = d_list
     #print("len",len(d_1))
     if 0.15 < t < 0.18:
-	x_2 = x_list
-	y_2 = y_list
+        x_2 = x_list
+        y_2 = y_list
         d_2 = d_list
     #print(d_2)
      #for i in range(len(d_1)):
-#	if  abs(d_1[i] - d_2[i]) > 0.01:
-#		x.append(x_1[i])
-#		y.append(y_1[i])
-#		x_n.append(x_2[i])
-#		y_n.append(y_2[i])
+#       if  abs(d_1[i] - d_2[i]) > 0.01:
+#               x.append(x_1[i])
+#               y.append(y_1[i])
+#               x_n.append(x_2[i])
+#               y_n.append(y_2[i])
     if t>0.18:
         print('Done')
 
@@ -216,8 +224,7 @@ def compare(x_1,y_1,d_1,x_2,y_2,d_2):
 
 
 def main():
-    #TODO: change to frequency
-    rate = rospy.Rate(39) # 30 [Hz]
+    rate = rospy.Rate(10) # 39 [Hz]
     mocap_sub = rospy.Subscriber('odometry_body_frame', Odometry, callback_mocap)
     lidar_sub = rospy.Subscriber('scan', LaserScan, callback_lidar)
     rospy.spin()
--- catkin_ws/src/TBD5/src/dynamic_obstacle_detection_idris.py
+++ catkin_ws/src/TBD5/src/dynamic_obstacle_detection_idris.py
@@ -9,6 +9,10 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 
 rospy.init_node('people_detection')
+dynamic_scan_pub = rospy.Publisher('dynamic_scan', LaserScan, queue_size=1)
+
+curr_scan = None
+dynamic_lookahead = 3.5
 
 ranges=[]
 x_list=[]#
@@ -26,12 +30,21 @@ d2_list=[]
 t=0
 
 def compare(x_1,y_1,d_1,x_2,y_2,d_2):
+    global curr_scan
+
     walking = False
     obstacle_counter=0
     x_vel_list=[]
     y_vel_list = []
+
+    dynamic_indices = []
     for i in range(len(x_1)):
-        if abs(d_1[i]-d_2[i]) > 0.2: #use x position diff
+        # if abs(d_1[i]-d_2[i]) > 0.2: #use x position diff
+        # if abs(d_1[i]-d_2[i]) > 0.2: #use x position diff
+        if ((abs(x_1[i]-x_2[i]) > 0.1 and
+             abs(x_1[i]-x_2[i]) < 0.3)  or
+            (abs(y_1[i]-y_2[i]) > 0.1 and
+             abs(y_1[i]-y_2[i]) < 0.3)): #use y position diff
             #print ('walking person x')
             #x.append(x_1[i])
             x_currvel=(x_1[i]-x_2[i])/0.8
@@ -44,6 +57,7 @@ def compare(x_1,y_1,d_1,x_2,y_2,d_2):
             #y_n.append(y_2[i])
             #walking = True #return true if somone is moving
             obstacle_counter = obstacle_counter + 1
+            dynamic_indices.append(i)
         #if abs(y_1[i]-y_2[i]) > 0.3: #use y position diff
             #print ('walking person y')
             #obstacle_counter=obstacle_counter+1
@@ -59,10 +73,36 @@ def compare(x_1,y_1,d_1,x_2,y_2,d_2):
         x_velocity = sum(x_vel_list)/len(x_vel_list)
         y_veloctiy = sum(y_vel_list)/len(y_vel_list)
         #print('walking person counter')
+
+        # publish moving obstacles
+        # dynamic_scan = curr_scan
+        dynamic_scan = LaserScan()
+        dynamic_scan.header.stamp = rospy.Time.now()
+        dynamic_scan.header.frame_id = "SVEA5"
+        dynamic_scan.angle_min = curr_scan.angle_min
+        dynamic_scan.angle_max = curr_scan.angle_max
+        dynamic_scan.angle_increment = curr_scan.angle_increment
+        dynamic_scan.scan_time = curr_scan.scan_time
+        dynamic_scan.range_min = curr_scan.range_min
+        dynamic_scan.range_max = curr_scan.range_max
+        # dynamic_ranges_list = list(dynamic_scan.ranges)
+        dynamic_ranges_list = list(curr_scan.ranges)
+        for i in range(len(curr_scan.ranges)):
+            if not i in dynamic_indices or curr_scan.ranges[i] > dynamic_lookahead:
+            # if i in dynamic_indices:
+                dynamic_ranges_list[i] = float("inf")
+                # dynamic_ranges_list.append(curr_scan.ranges[i])
+
+        # dynamic_scan = LaserScan()
+        dynamic_scan.ranges = tuple(dynamic_ranges_list)
+        dynamic_scan_pub.publish(dynamic_scan)
+
     return walking, x_velocity, y_velocity
 
 
 def callback_mocap(odometry_msg):
+    global curr_scan
+
     global ranges
     global x_list
     global y_list
@@ -77,7 +117,8 @@ def callback_mocap(odometry_msg):
     global y2_list
     global d2_list
 
-    if not len(ranges) == 0:
+    # if not len(ranges) == 0:
+    if not curr_scan is None:
         x_pos = odometry_msg.pose.pose.position.x
         y_pos = odometry_msg.pose.pose.position.y
         robot_yaw = odometry_msg.pose.pose.orientation.z
@@ -88,9 +129,9 @@ def callback_mocap(odometry_msg):
         x2_list = []
         y2_list = []
         d2_list = []
-        for i in range(len(ranges)):
-            o_distance = ranges[i] # ranges distance
-            o_angle = angle_min + i * increment + robot_yaw #angle to object (check if this works)
+        for i in range(len(curr_scan.ranges)):
+            o_distance = curr_scan.ranges[i] # ranges distance
+            o_angle = curr_scan.angle_min + i * curr_scan.angle_increment + robot_yaw #angle to object (check if this works)
             x_o = np.cos(o_angle) * o_distance #body coordinates of object
             y_o = np.sin(o_angle) * o_distance
             x_g = x_o + x_pos #global coordinates of object
@@ -100,12 +141,12 @@ def callback_mocap(odometry_msg):
             y_list.append(y_g)
             d_list.append(d)
 
-        if t<0.1: #want first lists near start of time
+        if t<0.05: #want first lists near start of time
             x1_list = x_list  # first comparison lists
             y1_list = y_list
             d1_list = d_list
 
-        if t%0.8 <0.1: #refresh x2, y2 and d2 every 0.5 seconds
+        if t%0.1 < 0.01: #refresh x2, y2 and d2 every 0.5 seconds
             x2_list = x_list
             y2_list = y_list
             d2_list = d_list
@@ -121,17 +162,20 @@ def callback_mocap(odometry_msg):
                 print('y velocity', y_velocity)
 
 def callback_lidar(scan):
-    global ranges
-    global angle_min
-    global increment
+    # global ranges
+    # global angle_min
+    # global increment
     global t
-    global scan_time
+    global curr_scan
+    # global scan_time
 
-    ranges = scan.ranges
-    angle_min = scan.angle_min
-    increment = scan.angle_increment
+    # ranges = scan.ranges
+    # angle_min = scan.angle_min
+    # increment = scan.angle_increment
     scan_time = scan.scan_time
     t += scan_time
+
+    curr_scan = scan
 
 
 def main():
