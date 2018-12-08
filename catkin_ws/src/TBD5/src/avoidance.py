d983c01970200d84936dae79ed21beb22e0a4d24
FedeFerro <fferro@kth.se>
2018-12-08 19:09:56 +0100

    comments on avoidance

--- catkin_ws/src/TBD5/src/avoidance.py
+++ catkin_ws/src/TBD5/src/avoidance.py
@@ -51,38 +51,51 @@ class State:
 ##############
 
 def pure_pursuit_control(state, cx, cy, pind): #cx, cy are the trajectories we want to follow
-
+    """
+    Given a trajectory, it computes the curvature delta that is necessary to define 
+    the steering of the car. The curvature delta is defined each time according to 
+    a particular point in the trajectory tx, ty. 
+    """
 
     ind = pind
-    tx = cx[ind]
-    ty = cy[ind]
+    tx = cx[ind]   # list of the x coordinates of the goal point
+    ty = cy[ind]   # list of the y coordinates of the goal point
     alpha = math.atan2(ty - state.y, tx - state.x) - state.yaw
 
     if state.v < 0:  # back
         alpha = math.pi - alpha
 
-    Lf = k * state.v + Lfc
+    Lf = k * state.v + Lfc  #proportional controller for velocity
 
     delta = math.atan2(2.0 * L * math.sin(alpha) / Lf, 1.0)
 
     return delta, ind
 
-#define index for path planning
+
 def calc_target_index(state, cx, cy):
+    """
+    Defines the index index to select a point in the trajectory. This point is then 
+    used by the pure pursuit to compute the curvature delta. 
+    """
     ind=1
     d=0.2
     dx = state.x - cx[ind]
     dy = state.y - cy[ind]
-    Ln = math.sqrt(dx ** 2 + dy ** 2)
+    Ln = math.sqrt(dx ** 2 + dy ** 2)  #distance between the position of the car and the goal point
     if d < Ln:
-        ind = 1
+        ind = 1   #since the trajectory is a list that contains repetition of the goal point, if the
+                  #distance between the car and the goal point is bigger than a certain treshold, the
+                  #first element of the trajectory is taken
     else:
-        ind = len(cx)+1
+        ind = len(cx)+1   #to stop computing the curvature
     print ind
     print Ln
     return ind
 
 def dist_target(state, cx, cy):
+    """
+    Computes the distance of the car from the goal point in cx,cy 
+    """
     dx = state.x - cx[1]
     dy = state.y - cy[1]
     dist_tg = math.sqrt(dx **2 + dy **2)
@@ -92,7 +105,7 @@ def dist_target(state, cx, cy):
 # ROS #
 #######
 
-rospy.init_node('pp_lidar_controller')
+rospy.init_node('pp_lidar_controller')   #initialization of the node
 ctrl_pub= rospy.Publisher('/lli/ctrl_request',lli_ctrl_request,queue_size=1)
 target_pub = rospy.Publisher('pure_pursuit_target_pose', PointStamped, queue_size=1)
 
@@ -107,10 +120,14 @@ d_min = 0.1
 
 
 def callback_mocap(odometry_msg):
+    """
+    Callback to obstain information from the Mocap. This function uses the pure pursuit when no obstacles
+    are detected, provides an emergy stop and performs obstacle avoidance 
+    """   
     global ranges
     global d_min
-    if not len(traj_x) == 0 and not len(ranges) == 0:
-        x_pos = odometry_msg.pose.pose.position.x
+    if not len(traj_x) == 0 and not len(ranges) == 0:    #unpacking information obtained from the qualisys system
+        x_pos = odometry_msg.pose.pose.position.x 
         y_pos = odometry_msg.pose.pose.position.y
         yaw = odometry_msg.pose.pose.orientation.z
         v = odometry_msg.twist.twist.linear.x
@@ -122,7 +139,7 @@ def callback_mocap(odometry_msg):
 
         dist_tg = dist_target(state_m, traj_x, traj_y)
 
-        if min_dist < 0.6 and dist_tg > d_min:
+        if min_dist < 0.6 and dist_tg > d_min: #if an obstacle is found and the car is far enough from the goal point the velocity is reduced
             angle_list = []
             target_speed = 20
             control_request = lli_ctrl_request()
@@ -134,18 +151,19 @@ def callback_mocap(odometry_msg):
                 angle = angle_min + i * increment
                 angle_list.append(angle)
 
-            # Want the car to turn 45[deg] so here it calculates [rad] since LidarScan and car uses [rad] then mutliply by 100 because the car takes in percentage to steer
-                if -(90*math.pi/180) <= angle_list[i] <= -(70*math.pi/180):
-                    if ranges[i] < 0.2:
-                        y_steering =180*0.4751131*(abs(angle_list[i]))/(math.pi) - 59.23077
+            
+                if -(90*math.pi/180) <= angle_list[i] <= -(70*math.pi/180):  # the car turn 45[deg] converted in [rad] since LidarScan and car uses [rad] and then multliplied 
+                                                                             # by 100 because the car takes in percentage to steer
+
+                    if ranges[i] < 0.2:   #something is detected in the selected range of angles
+                        y_steering =180*0.4751131*(abs(angle_list[i]))/(math.pi) - 59.23077   #parameters that give the steering of the car tuned on simulation
                         if angle_list[i] < 0:
-                            y_steering = -y_steering
+                            y_steering = -y_steering   #to ensure the car streers on the right side accordig to the poistion of the detected obstacle
                         control_request = lli_ctrl_request()
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = (15*math.pi/180)*100
-
+           
 
                 if -(70*math.pi/180) < angle_list[i] <= -(50*math.pi/180):
                     if ranges[i] < 0.3:
@@ -156,7 +174,7 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = (25*math.pi/180)*100
+                      
 
                 if -(50*math.pi/180) < angle_list[i] <= -(30*math.pi/180):
                     if ranges[i] < 0.4:
@@ -167,7 +185,7 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = (35*math.pi/180)*100
+                        
 
                 if -(30*math.pi/180) < angle_list[i] <= -(10*math.pi/180):
                     if ranges[i] < 0.5:
@@ -178,7 +196,7 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = (45*math.pi/180)*100
+                       
 
                 if -(10*math.pi/180) < angle_list[i] <= (10*math.pi/180):
                     if ranges[i] < 0.6:
@@ -189,9 +207,9 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = -(55*math.pi/180)*100
-                    #else:
-                        if ranges[i] < 0.2:
+                
+                    
+                        if ranges[i] < 0.2:    #sort of emergency stop: the car is too close to steer and just stops
                             while min_dist < 0.2:
                                 control_request = lli_ctrl_request()
                                 control_request.velocity = 0
@@ -206,8 +224,7 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = -(45*math.pi/180)*100
-
+                       
                 if (30*math.pi/180) < angle_list[i] <= (50*math.pi/180):
                     if ranges[i] < 0.4:
                         y_steering =180*0.4751131*(abs(angle_list[i]))/(math.pi) - 59.23077
@@ -215,7 +232,7 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = -(35*math.pi/180)*100
+                       
 
                 if (50*math.pi/180) < angle_list[i] <= (70*math.pi/180):
                     if ranges[i] < 0.3:
@@ -224,7 +241,7 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = -(25*math.pi/180)*100
+                        
 
                 if (70*math.pi/180) <= angle_list[i] <= (90*math.pi/180):
                     if ranges[i] < 0.2:
@@ -233,10 +250,10 @@ def callback_mocap(odometry_msg):
                         control_request.velocity = target_speed
                         control_request.steering = (y_steering/1.25 * math.pi / 180) * 100
                         ctrl_pub.publish(control_request)
-                        #control_request.steering = -(15*math.pi/180)*100
+                        
                     
 
-        elif min_dist < 0.15:
+        elif min_dist < 0.15:    #emergency stop
             while min_dist < 0.2:
                 control_request = lli_ctrl_request()
                 control_request.velocity = 0
